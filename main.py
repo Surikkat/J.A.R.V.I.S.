033ce58e319404705e824883140066eb58d26943
Surikkat <Surikkatik@gmail.com>
2023-07-28 12:37:25 +0300

    Режим спячки и несколько новых команд

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import datetime
 import speech_recognition as sr
 import wikipedia
 import webbrowser
+import winsound
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
 
 def greeting():
 
@@ -28,7 +31,9 @@ def greeting():
 
     speak("Я голосовой помошник - Пятница.")
 
+
 def get_username():
+
     speak("Как я могу к тебе обращаться?")
     username = hear()
 
@@ -41,59 +46,95 @@ def get_username():
 
     return username
 
+
 def hear():
 
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
+        if awake_flag:
+            winsound.PlaySound("*", winsound.SND_ALIAS)
+        r.pause_threshold = 1
+        audio = r.listen(source)
 
-        r.pause_threshold = 2
-        audio = r.record(source, 3)
+    try:
+        query = r.recognize_google(audio, language='ru-ru')
+        print(query)
 
-        try:
-            query = r.recognize_google(audio, language='ru-ru')
-            print(query)
-
-        except Exception as e:
-            print(e)
-
-        return None
+    except Exception as e:
+        print(e)
+        return "None"
 
     return query
 
 
-
-
 if __name__ == '__main__':
 
-    #greeting()
-    #username = get_username()
+    os.system('cls')
 
-    speak("Как я могу помочь тебе?")
+    with sr.Microphone():
 
-    while True:
+        awake_flag = True
 
-        command = hear()
+        greeting()
+        username = get_username()
 
-        if command is not None:
-            command = command.lower()
+        while True:
 
-            if 'открой википедию' in command:
-                speak("Что мне найти в википедии?")
-                ask = hear().lower()
-                wikipedia.set_lang("ru")
-                results = wikipedia.summary(ask, sentences=3)
-                speak("На википедии говориться")
-                speak(results)
-            elif 'открой ютуб' in command:
-                speak("Открываю ютуб в твоём браузере:")
-                webbrowser.open("youtube.com")
-            elif 'открой гугл' in command:
-                speak("Открываю гугл в твоём браузере:")
-                webbrowser.open("google.com")
-            elif 'сколько время' in command:
-                strTime = datetime.datetime.now().strftime("%H:%M:%S")
-                speak(f"Сейчас {strTime}")
+            command = hear()
+
+            if 'пятница' in command:
+                awake_flag = True
+
+            if awake_flag:
+
+                if command is not None:
+                    command = command.lower()
+
+                    if 'открой википедию' in command:
+
+                        webbrowser.open("wikipedia.com")
+
+                        speak("Что мне найти в википедии?")
+                        ask = hear().lower()
+
+                        wikipedia.set_lang("ru")
+                        results = wikipedia.summary(ask, sentences=3)
+
+                        speak("На википедии говориться")
+                        speak(results)
+
+                    elif 'открой youtube' in command:
+
+                        speak("Открываю ютуб в твоём браузере:")
+                        webbrowser.open("youtube.com")
+
+                    elif 'открой google' in command:
+
+                        speak("Открываю гугл в твоём браузере:")
+                        webbrowser.open("google.com")
+
+                    elif 'открой гитхаб' in command:
+
+                        speak("Открываю гитхаб в твоём браузере:")
+                        webbrowser.open("github.com")
+
+                    elif 'открой браузер' in command:
+
+                        path = r"C:\Program Files\Internet Explorer\iexplore.exe"
+                        os.startfile(path)
+
+                    elif 'сколько время' in command:
+
+                        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+                        speak(f"Сейчас {strTime}")
+
+                    elif 'пока' in command:
+                        speak("Услышимся в следующий раз")
+                        awake_flag = False
+
+
+
 
 
 
